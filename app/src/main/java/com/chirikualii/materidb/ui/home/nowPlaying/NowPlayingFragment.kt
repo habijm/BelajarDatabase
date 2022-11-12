a69143ef25eba111a7d523fada1df8441d8d44c2
Habi <habijiyan111@gmail.com>
2022-11-12 15:02:03 +0700

    Membuat Search Layout

--- app/src/main/java/com/chirikualii/materidb/ui/home/nowPlaying/NowPlayingFragment.kt
+++ app/src/main/java/com/chirikualii/materidb/ui/home/nowPlaying/NowPlayingFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.fragment.app.viewModels
 import com.chirikualii.materidb.R
@@ -40,6 +41,9 @@ class NowPlayingFragment : Fragment() {
         adapter = MovieListAdapter()
         binding.rvMovie.adapter = adapter
 
+        binding.tvSearch.setOnClickListener {
+            Toast.makeText(requireContext(), "search page", Toast.LENGTH_SHORT).show()
+        }
 
         observeView()
     }
